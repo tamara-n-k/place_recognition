247c0b69a3c30347d36e707fbbd74131e6adaf98
Tamara Kraus <tamara.kraus@stud.tu-darmstadt.de>
2026-01-31 17:00:57 +0100

    changed to data loader from nclt

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,10 +1,37 @@
 import numpy as np
 import os
+import struct
 
 def load_nclt_scan(bin_path):
-    data = np.fromfile(bin_path, dtype=np.float32)
-    data = data.reshape(-1, 4)
-    return data[:, :3]
+    """
+    Loads Velodyne .bin
+    Returns Nx3 numpy array [m]
+    """
+    hits = []
+    scaling = 0.005  # 5 mm
+    offset = -100.0
+
+    with open(bin_path, "rb") as f:
+        while True:
+            x_bytes = f.read(2)
+            if len(x_bytes) < 2:
+                break  # EOF
+
+            x = struct.unpack('<H', x_bytes)[0]
+            y = struct.unpack('<H', f.read(2))[0]
+            z = struct.unpack('<H', f.read(2))[0]
+            i = struct.unpack('B', f.read(1))[0]  # intensity
+            l = struct.unpack('B', f.read(1))[0]  # laser id (ignore)
+
+            x_m = x * scaling + offset
+            y_m = y * scaling + offset
+            z_m = z * scaling + offset
+
+            hits.append([x_m, y_m, z_m])
+
+    return np.array(hits)
+
+
 
 def scan_generator(scan_dir):
     files = sorted(os.listdir(scan_dir))
