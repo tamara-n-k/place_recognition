6149506d7f4192b74bf954de0fc9de0ce1974558
Tamara Kraus <tamara.kraus@stud.tu-darmstadt.de>
2026-01-31 17:47:54 +0100

    nearest neighbour similarity

--- src/similarity.py
+++ src/similarity.py
@@ -1,12 +1,32 @@
 import numpy as np
 
-def scan_context_distance(desc1, desc2):
+def scan_context_nn_distance(desc1, desc2):
     min_dist = np.inf
     num_sectors = desc1.shape[1]
 
     for shift in range(num_sectors):
         shifted = np.roll(desc2, shift, axis=1)
-        dist = np.linalg.norm(desc1 - shifted)
+        dist = nn_column_distance(desc1, shifted)
         min_dist = min(min_dist, dist)
 
     return min_dist
+
+
+def nn_column_distance(desc1, desc2):
+    """
+    Nearest-neighbor column distance between two descriptors
+    """
+    num_sectors = desc1.shape[1]
+    total_dist = 0.0
+
+    for j in range(num_sectors):
+        col1 = desc1[:, j]
+
+        dists = np.linalg.norm(
+            desc2 - col1[:, None],
+            axis=0
+        )
+
+        total_dist += np.min(dists)
+
+    return total_dist / num_sectors
