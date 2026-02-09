831675e5a69570056eeedfacc2e09653baf74f45
Tamara Kraus <tamara.kraus@stud.tu-darmstadt.de>
2026-02-09 23:56:48 +0100

    yaw invariant L1 similarity metric

--- src/similarity.py
+++ src/similarity.py
@@ -1,32 +1,15 @@
 import numpy as np
 
-def scan_context_nn_distance(desc1, desc2):
+def scan_context_nn_distance(sc1, sc2):
+    num_sectors = sc1.shape[1]
     min_dist = np.inf
-    num_sectors = desc1.shape[1]
-
+    
+    # We must try all shifts to be yaw-invariant
     for shift in range(num_sectors):
-        shifted = np.roll(desc2, shift, axis=1)
-        dist = nn_column_distance(desc1, shifted)
-        min_dist = min(min_dist, dist)
-
+        sc2_shifted = np.roll(sc2, shift, axis=1)
+        # L1 distance is the SC standard
+        dist = np.mean(np.abs(sc1 - sc2_shifted))
+        
+        if dist < min_dist:
+            min_dist = dist
     return min_dist
-
-
-def nn_column_distance(desc1, desc2):
-    """
-    Nearest-neighbor column distance between two descriptors
-    """
-    num_sectors = desc1.shape[1]
-    total_dist = 0.0
-
-    for j in range(num_sectors):
-        col1 = desc1[:, j]
-
-        dists = np.linalg.norm(
-            desc2 - col1[:, None],
-            axis=0
-        )
-
-        total_dist += np.min(dists)
-
-    return total_dist / num_sectors
